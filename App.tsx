9dc5538e3756face690bcb51a93f5b4feefb4bae
jhaemin <io@jhaemin.com>
2020-08-30 15:30:33 +0900

    Revamp the application entirely
    * use the Expo SDK(v38) instead of bare React Native
    * use appearance module to support dark mode seamlessly
    * feat: add webview navigation
    * push notifications are temporarily omitted

---
+++ App.tsx
@@ -0,0 +1,104 @@
+import { StatusBar } from 'expo-status-bar'
+import React, { useRef, useState } from 'react'
+import { StyleSheet, View, TouchableOpacity } from 'react-native'
+import { WebView, WebViewNavigation } from 'react-native-webview'
+import Constants from 'expo-constants'
+import { AppearanceProvider, useColorScheme } from 'react-native-appearance'
+import { Feather } from '@expo/vector-icons'
+import * as Linking from 'expo-linking'
+
+function App() {
+  const colorScheme = useColorScheme()
+  const webView = useRef<WebView | null>()
+  const [
+    navigationState,
+    setNavigationState,
+  ] = useState<WebViewNavigation | null>(null)
+
+  return (
+    <View style={styles.container}>
+      <StatusBar style="auto" />
+      <View
+        style={{
+          height: Constants.statusBarHeight,
+          backgroundColor: colorScheme === 'light' ? '#f0f2f3' : '#000000',
+        }}
+      />
+      <WebView
+        source={{ uri: 'https://eodiro.com' }}
+        decelerationRate="normal"
+        style={styles.webView}
+        ref={(wv) => (webView.current = wv)}
+        onNavigationStateChange={(navigation) => {
+          setNavigationState(navigation)
+
+          if (!navigation.url.startsWith('https://eodiro.com')) {
+            webView.current?.stopLoading()
+            Linking.openURL(navigation.url)
+          }
+        }}
+      />
+      <View
+        style={{
+          display: 'flex',
+          flexDirection: 'row',
+          backgroundColor: colorScheme === 'light' ? '#f0f2f3' : '#000000',
+        }}
+      >
+        <TouchableOpacity
+          style={{ ...styles.navigationButton, paddingLeft: 10 }}
+          onPress={() => {
+            webView.current?.goBack()
+          }}
+        >
+          <Feather
+            name="chevron-left"
+            size={30}
+            color={colorScheme === 'light' ? '#000' : '#fff'}
+            style={{
+              opacity: navigationState?.canGoBack ? 1 : 0.2,
+            }}
+          />
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={{ ...styles.navigationButton, marginLeft: 10 }}
+          onPress={() => {
+            webView.current?.goForward()
+          }}
+        >
+          <Feather
+            name="chevron-right"
+            size={30}
+            color={colorScheme === 'light' ? '#000' : '#fff'}
+            style={{
+              opacity: navigationState?.canGoForward ? 1 : 0.2,
+            }}
+          />
+        </TouchableOpacity>
+      </View>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    width: '100%',
+    height: '100%',
+  },
+  webView: {},
+  navigationButton: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: 50,
+    height: 50,
+  },
+})
+
+export default function Main() {
+  return (
+    <AppearanceProvider>
+      <App />
+    </AppearanceProvider>
+  )
+}
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 <h1 align="center">eodiro App</h1>
+
+The hybrid **eodiro** mobile webview application for both iOS and Android.
