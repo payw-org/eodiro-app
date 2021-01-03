604ba5adfef57948eca22fb2b08b0c6ddd4cada4
Jang Haemin <io@jhaemin.com>
2021-01-03 15:22:38 +0900

    feat: notice notifications

--- App.tsx
+++ App.tsx
@@ -1,20 +1,124 @@
-import { StatusBar } from 'expo-status-bar'
-import React, { useRef, useState } from 'react'
-import { StyleSheet, View, TouchableOpacity } from 'react-native'
-import { WebView, WebViewNavigation } from 'react-native-webview'
-import Constants from 'expo-constants'
-import { AppearanceProvider, useColorScheme } from 'react-native-appearance'
 import { Feather } from '@expo/vector-icons'
+import { Subscription } from '@unimodules/core'
+import Constants from 'expo-constants'
 import * as Linking from 'expo-linking'
+import * as Notifications from 'expo-notifications'
+import * as Permissions from 'expo-permissions'
+import { StatusBar } from 'expo-status-bar'
+import React, { useEffect, useRef, useState } from 'react'
+import {
+  Animated,
+  Image,
+  Platform,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native'
+import { AppearanceProvider, useColorScheme } from 'react-native-appearance'
 import { getBottomSpace } from 'react-native-iphone-x-helper'
+import { WebView, WebViewNavigation } from 'react-native-webview'
+import { env } from './env'
+
+const isDev = process.env.NODE_ENV === 'development'
+const eodiroUrl = isDev ? `http://${env.IP}:3020` : 'https://eodiro.com'
+
+// Notifications
+Notifications.setNotificationHandler({
+  handleNotification: async () => ({
+    shouldShowAlert: true,
+    shouldPlaySound: true,
+    shouldSetBadge: true,
+  }),
+})
+
+async function registerForPushNotificationsAsync() {
+  let token
+  if (Constants.isDevice) {
+    const { status: existingStatus } = await Permissions.getAsync(
+      Permissions.NOTIFICATIONS
+    )
+    let finalStatus = existingStatus
+    if (existingStatus !== Permissions.PermissionStatus.GRANTED) {
+      const { status } = await Permissions.askAsync(Permissions.NOTIFICATIONS)
+      finalStatus = status
+    }
+    if (finalStatus !== Permissions.PermissionStatus.GRANTED) {
+      alert('Failed to get push token for push notification!')
+      return
+    }
+    token = (await Notifications.getExpoPushTokenAsync()).data
+  } else {
+    alert('Must use physical device for Push Notifications')
+  }
+
+  if (Platform.OS === 'android') {
+    Notifications.setNotificationChannelAsync('default', {
+      name: 'default',
+      importance: Notifications.AndroidImportance.MAX,
+      vibrationPattern: [0, 250, 250, 250],
+      lightColor: '#FF231F7C',
+    })
+  }
+
+  return token
+}
 
 function App() {
+  const [expoPushToken, setExpoPushToken] = useState<string | undefined>('')
+  const [
+    notification,
+    setNotification,
+  ] = useState<Notifications.Notification | null>(null)
+  const notificationListener = useRef<Subscription>({ remove: () => {} })
+  const responseListener = useRef<Subscription>({ remove: () => {} })
+
+  const [isLoaded, setIsLoaded] = useState(false)
+  const [hasError, setHasError] = useState(false)
+
   const colorScheme = useColorScheme()
   const webView = useRef<WebView | null>()
   const [
     navigationState,
     setNavigationState,
   ] = useState<WebViewNavigation | null>(null)
+
+  const fadeAnim = useRef(new Animated.Value(1)).current
+
+  useEffect(() => {
+    registerForPushNotificationsAsync().then((token) => {
+      webView.current?.injectJavaScript(`
+        window.expoPushToken = '${token}';
+      `)
+      setExpoPushToken(token)
+    })
+
+    // This listener is fired whenever a notification is received
+    // while the app is foregrounded
+    notificationListener.current = Notifications.addNotificationReceivedListener(
+      (notification) => {
+        setNotification(notification)
+      }
+    )
+
+    // This listener is fired whenever a user taps on or interacts
+    // with a notification (works when app is foregrounded, backgrounded, or killed)
+    responseListener.current = Notifications.addNotificationResponseReceivedListener(
+      (response) => {
+        const { data } = response.notification.request.content
+
+        if (data.type === 'notice' && data.url) {
+          Linking.openURL(data.url as string)
+        }
+        console.log(response)
+      }
+    )
+
+    return () => {
+      Notifications.removeNotificationSubscription(notificationListener.current)
+      Notifications.removeNotificationSubscription(responseListener.current)
+    }
+  }, [])
 
   return (
     <View style={styles.container}>
@@ -26,14 +130,33 @@ function App() {
         }}
       />
       <WebView
-        source={{ uri: 'https://eodiro.com' }}
+        source={{
+          uri: eodiroUrl,
+          headers: {
+            'eodiro-agent': 'expo',
+          },
+        }}
+        onLoad={() => {
+          Animated.timing(fadeAnim, {
+            toValue: 0,
+            duration: 600,
+            useNativeDriver: true,
+          }).start()
+        }}
+        onError={() => {
+          setHasError(true)
+        }}
+        onMessage={() => {}}
+        injectedJavaScript={`
+          window.isApp = true;
+        `}
         decelerationRate="normal"
         style={styles.webView}
         ref={(wv) => (webView.current = wv)}
         onNavigationStateChange={(navigation) => {
           setNavigationState(navigation)
 
-          if (!navigation.url.startsWith('https://eodiro.com')) {
+          if (!navigation.url.startsWith(eodiroUrl)) {
             webView.current?.stopLoading()
             Linking.openURL(navigation.url)
           }
@@ -44,7 +167,7 @@ function App() {
           display: 'flex',
           flexDirection: 'row',
           backgroundColor: colorScheme === 'light' ? '#f0f2f3' : '#000000',
-          paddingBottom: getBottomSpace()
+          paddingBottom: getBottomSpace(),
         }}
       >
         <TouchableOpacity
@@ -78,6 +201,49 @@ function App() {
           />
         </TouchableOpacity>
       </View>
+      <Animated.View
+        pointerEvents="none"
+        style={{
+          position: 'absolute',
+          top: 0,
+          left: 0,
+          width: '100%',
+          height: '100%',
+          backgroundColor: '#000000',
+          opacity: fadeAnim,
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+        }}
+      >
+        {hasError && (
+          <View
+            style={{
+              display: 'flex',
+              flexDirection: 'column',
+              alignItems: 'center',
+            }}
+          >
+            <Image
+              source={require('./assets/icon.png')}
+              style={{
+                width: 100,
+                height: 100,
+                marginBottom: 50,
+              }}
+            />
+            <Text style={{ color: '#fff', lineHeight: 20 }}>
+              서버에 접속하지 못했습니다.
+            </Text>
+            <Text style={{ color: '#fff', lineHeight: 20 }}>
+              문제가 지속될 시,
+            </Text>
+            <Text style={{ color: '#fff', lineHeight: 20 }}>
+              support@eodiro.com으로 문의해주세요.
+            </Text>
+          </View>
+        )}
+      </Animated.View>
     </View>
   )
 }
